32a28a5406aa0097158883ca9a2c19eddfce089f
dineshargamla <dineshargamla@gmail.com>
2023-10-16 15:24:58 +0530

    adding allure report

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,8 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'allure-playwright',
+  // globalSetup: './global-setup',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     //BaseURL
